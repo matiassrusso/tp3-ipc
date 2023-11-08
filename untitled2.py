0b51572b92b603571575c9930c810b494712cf03
matias russo <73359875+matiassrusso@users.noreply.github.com>
2023-11-08 16:05:59 -0300

    varios cambios

--- untitled2.py
+++ untitled2.py
@@ -7,3 +7,26 @@ Created on Tue Nov  7 11:45:24 2023
 """
 
 #TP3 IPC
+simbolo_repartida="~"
+lista=[]
+
+
+with open("corto.txt","r") as corto:
+    for linea in corto:
+        texto=linea.strip().split(",")
+        lista.extend(texto) #Lista es un array que guarda todas las lineas del texto.
+        
+    
+               
+my_dic={}
+for nombre in lista[0].split(" "):
+    my_dic[nombre]=0
+
+
+#print(my_dic)
+
+for linea in lista:
+    linea=linea.split(" ")
+    if simbolo_repartida in linea:
+        print("god")
+#print(lista[1])
