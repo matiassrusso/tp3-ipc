5baf5a5ee327de593fd242a35f24c6ae10b98ec8
matias russo <73359875+matiassrusso@users.noreply.github.com>
2023-11-13 16:02:34 -0300

    mucho cambio

--- prueba.py
+++ prueba.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Nov 11 13:18:29 2023
-
-@author: matia
-"""
-
 from collections import defaultdict
 
 def leer_archivo(nombre_archivo):
@@ -26,25 +19,29 @@ def procesar_linea(linea, inquilinos, deudas):
         if '~' in partes:  # Repartir deuda entre inquilinos
             no_pagan = set(partes[4:])
             pagan = inquilinos - no_pagan
-            monto_por_persona = monto / len(pagan)
             
-            for persona in pagan:
-                deudas[persona] += monto_por_persona
+            if pagan:  # Verificar que haya inquilinos que deban pagar
+                monto_por_persona = monto / len(pagan)
+                
+                for persona in pagan:
+                    deudas[persona] += monto_por_persona
         else:
             involucrados = set(partes[3:])
-            monto_por_persona = monto / len(involucrados)
             
-            for persona in involucrados:
-                deudas[persona] += monto_por_persona
+            if involucrados:  # Verificar que haya inquilinos involucrados
+                monto_por_persona = monto / len(involucrados)
+                
+                for persona in involucrados:
+                    deudas[persona] += monto_por_persona
 
 def main():
     archivo_largo = "transacciones_largo.txt"
-    archivo_corto = "transacciones_corto.txt"
+    #archivo_corto = "transacciones_simple.txt"
     
     inquilinos = set()
     deudas = defaultdict(float)
     
-    for archivo in [archivo_largo, archivo_corto]:
+    for archivo in [archivo_largo]: #agregar archivos_corto
         lineas = leer_archivo(archivo)
         
         for linea in lineas[1:]:  # Ignorar la primera línea con los nombres
@@ -53,8 +50,6 @@ def main():
     print("Total deudas por inquilino:")
     for persona, deuda in deudas.items():
         print(f"{persona}: ${deuda:.2f}")
-
-
-
+        
 if __name__ == "__main__":
     main()
--- untitled2.py
+++ untitled2.py
@@ -23,19 +23,61 @@ for nombre in lista[0].split(" "): #Nombramos las claves del diccionario
     deuda[nombre]=0
 #print(deuda)
 
+inquilinos = list(deuda.keys())
+def inquilino_nuevo(nuevo):
+    inquilinos.append(nuevo)
+    deuda[nuevo]=0
+    
+
 lista2 = []
 for i in range(len(lista)):
     lista2.append(lista[i].split(' '))
 
-#print (lista2)
+#print(deuda)
 
-def calculo_deuda(fecha)#Que reciba como argumento fecha (nose bien como hacer para que calcule hasta tal fecha)
-    i = 0
+for lineas in lista2[1:]:
+    if simbolo_excluye in lineas:
+        deuda[lineas[1]] = deuda[lineas[1]]-int(lineas[2])
+        pagan=int(lineas[2])/(len(deuda)-len(lineas[4:]))
+        deudores = []
+        for nombres in inquilinos:
+            if nombres not in lineas[4:]:
+                deudores.append(nombres)
+                for nombre in deudores:
+                    deuda[nombre]+=int(pagan)
+    elif simboolo_nuevo in lineas:
+        inquilino_nuevo(lineas[2])
+        
+
+                    
+            
+    else:
+        deuda[lineas[1]]=deuda[lineas[1]]-int(lineas[2])
+        pagan=int(lineas[2])/int(len(lineas[3:]))
+        for nombres in lineas[3:]:
+            deuda[nombres]+=int(pagan)
+print(deuda)
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+def calculo_deuda(fecha):#Que reciba como argumento fecha (nose bien como hacer para que calcule hasta tal fecha)
+    i = 1
     m = lista2[i][0]
     while m != fecha:
         i += 1 #salteo la primer lista dentro de lista2
-        if simbolo_excluye in lista[i]
+        if simbolo_excluye in lista[i]:
         
-         
-    
+"""       
     
