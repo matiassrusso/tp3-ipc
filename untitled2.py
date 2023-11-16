ff1ada0c84d309e28c46228e050cae5887a40162
Benciogravier <150157673+Benciogravier@users.noreply.github.com>
2023-11-16 19:53:21 -0300

    Update untitled2.py

--- untitled2.py
+++ untitled2.py
@@ -14,7 +14,7 @@ import matplotlib.pyplot as plt
 
 lista=[]
 
-with open("transacciones_simple.txt","r") as corto:
+with open("transacciones_largo.txt","r") as corto:
     for linea in corto:
         texto=linea.strip().split(",")
         lista.extend(texto) #Lista es un array que guarda todas las lineas del texto.
@@ -36,8 +36,6 @@ lista2 = [] #Esta segunda lista va a guardar todas las lineas del texto pero cad
 for i in range(len(lista)):
     lista2.append(lista[i].split(' '))
 
-#print(deuda)
-
 
 
 fechas_str = []
@@ -57,8 +55,9 @@ def calculo_deuda(year, month, day):   #Que recibe como argumento fecha
             u += 1
     return u
         
-prueba = calculo_deuda(2023, 9, 13)
-print(prueba)
+prueba = calculo_deuda(2029, 9, 13)
+
+#print(prueba)
 
 k=0
 
@@ -66,24 +65,23 @@ for lineas in lista2[1:prueba]: #calcula la deuda hasta el valor de u (cantidad 
     
     if simbolo_excluye in lineas:
         deuda[lineas[1]] = round (deuda[lineas[1]]-int(lineas[2]),2) #Se le resta al que pago en el diccionario
-        pagan=int(lineas[2])/(len(deuda)-len(lineas[4:])) #Se calcula en cada linea cuanto se debe pagar por persona
+        pagan= round (int(lineas[2])/(len(deuda)-len(lineas[4:])),2) #Se calcula en cada linea cuanto se debe pagar por persona
         deudores = []
         for nombres in inquilinos:
             if nombres not in lineas[4:]:
                 deudores.append(nombres)
         for nombre in deudores:
-            deuda[nombre]+=round (pagan,2)
+            deuda[nombre]+= round(pagan,2)
     
     elif simboolo_nuevo in lineas:
         inquilino_nuevo(lineas[2])
                                     
     else:
-        deuda[lineas[1]]= round (deuda[lineas[1]]-int(lineas[2]), 2)
-        pagan=int(lineas[2])/int(len(lineas[3:]))
+        deuda[lineas[1]]= round(deuda[lineas[1]]-int(lineas[2]), 2)
+        pagan = round(int(lineas[2])/int(len(lineas[3:])),2)
         for nombres in lineas[3:]:
-            deuda[nombres]+=round (pagan,2)
+            deuda[nombres]+= round(pagan,2)
     
-
     
 #print(deuda)
 
